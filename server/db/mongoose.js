d20348e669828f10f28838b8936b2acc2367698c
Matias <matias@tofful.com>
2016-12-30 19:31:38 +1300

    change to db conection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp'
 };
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect( db.mlab);
 
 module.exports = { mongoose };
