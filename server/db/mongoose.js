bf4ec012a61173d4e269729fb18273b43292c655
Matias <matias@tofful.com>
2016-12-30 16:17:24 +1300

    Add new db user and pass.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://tofful:doonde123@ds149268.mlab.com:49268/todoapp'
 };
-mongoose.connect(db.mlab || db.localhost);
+mongoose.connect('mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp');
 
 module.exports = { mongoose };
