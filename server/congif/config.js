2f5e1148c4194d5fc539594ba45eff026317b09f
Matias <matias@tofful.com>
2016-12-30 20:14:28 +1300

    Add code refactory

---
+++ server/congif/config.js
@@ -0,0 +1,20 @@
+let env = process.env.NODE_ENV || 'development';
+
+switch (env) {
+  case 'development':
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    break;
+  case 'test':
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    break;
+  // case 'production':
+  //     // Heroku
+  //     process.env.MONGODB_URI = 'mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp';
+  //     break;
+  default:
+    // Development by default
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp'
-};
-mongoose.connect(db.localhost || db.mlab);
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp'
+// };
+// mongoose.connect(db.localhost || db.mlab);
+
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
