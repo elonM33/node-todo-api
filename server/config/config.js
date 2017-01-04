93853423f0d18e66bd80e2a16df0c8c55cc8c1fd
Matias <matias@tofful.com>
2017-01-04 22:43:31 +1300

    Update config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,20 +1,29 @@
-let env = process.env.NODE_ENV || 'developments';
+let env = process.env.NODE_ENV || 'development';
 
-switch (env) {
-  case 'development':
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-    break;
-  case 'test':
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-    break;
-  case 'production':
-      // Heroku
-      process.env.MONGODB_URI = 'mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp';
-      break;
-  default:
-    // Development by default
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+if (env === 'development' || env === 'test') {
+  let config = require('./config.json');
+  let envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+// switch (env) {
+//   case 'development':
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+//     break;
+//   case 'test':
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+//     break;
+//   case 'production':
+//       // Heroku
+//       process.env.MONGODB_URI = 'mongodb://tofful:123456@ds149268.mlab.com:49268/todoapp';
+//       break;
+//   default:
+//     // Development by default
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// }
